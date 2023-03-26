3e88e40b11ddfb51d6817ebc380021cf7f2c18f3
GlacierLuo <1090490148@qq.com>
2023-03-26 20:56:33 +0800

    Add expand and shrink to footer console.

--- packages/core-dev/src/components/FooterConsole/index.tsx
+++ packages/core-dev/src/components/FooterConsole/index.tsx
@@ -1,26 +1,36 @@
-import {Allotment} from "allotment";
+import { Allotment } from "allotment";
+import { useState } from "react";
+import { Bars2Icon } from "@heroicons/react/24/outline";
 
-import {useEditor} from "../../editor/editorContext";
+import { useEditor } from "../../editor/editorContext";
 
 import CallBlock from "./CallBlock";
 import ConsoleBlock from "./ConsoleBlock";
 import "allotment/dist/style.css";
 
 const FooterConsole = () => {
-  const {id} = useEditor();
+  const { id } = useEditor();
+  const [display, setDisplay] = useState(true)
 
   return (
-    <div key={id + "_footerConsole"} className="flex bg-[#0f172a] text-white p-2 gap-2 rounded-b-lg h-1 min-h-[300px]">
-      <Allotment key={id + "_Allotment"}>
-        <div className="flex-1 flex-col">
-          <div>Execute contract:</div>
-          <CallBlock/>
-        </div>
-        <div className="flex-[2] flex flex-col ml-2">
-          <div>Console:</div>
-          <ConsoleBlock/>
-        </div>
-      </Allotment>
+    <div>
+      <div className="flex justify-center items-center bg-[#0f172a] text-zinc-50 h-10 cursor-pointer" onClick={() => setDisplay(!display)}>
+        {display
+          ? <Bars2Icon className="h-6 w-10 text-blue-500" preserveAspectRatio="none meet" />
+          : <Bars2Icon className="h-6 w-10 text-gray-500" preserveAspectRatio="none meet" />}
+      </div>
+      <div key={id + "_footerConsole"} className={`transition-all duration-500 ease flex bg-[#0f172a] text-white gap-2 rounded-b-lg p-2`} style={{height: display ? 300 : 0}}>
+        <Allotment key={id + "_Allotment"}>
+          <div className="flex-1 flex-col">
+            <div>Execute contract:</div>
+            <CallBlock />
+          </div>
+          <div className="flex-[2] flex flex-col ml-2">
+            <div>Console:</div>
+            <ConsoleBlock />
+          </div>
+        </Allotment>
+      </div>
     </div>
   )
 }
