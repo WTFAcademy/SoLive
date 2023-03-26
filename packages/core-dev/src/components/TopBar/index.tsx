a4979f0da2d1d159669f6984a2119af75d24c508
chenchongqiang <chongqiangchen@gmail.com>
2023-03-26 23:36:01 +0800

    fix(core-dev): fix mobile flex

--- packages/core-dev/src/components/TopBar/index.tsx
+++ packages/core-dev/src/components/TopBar/index.tsx
@@ -11,7 +11,7 @@ const TopBar = () => {
 
   return (
     <div className="h-[50px] rounded-t-lg bg-[#0f172a] flex items-center px-4 justify-between">
-      <div className="flex items-center gap-1">
+      <div className="flex items-center gap-1 w-4/5 overflow-auto">
         {models.map((model, index) => (
           <Nav
             key={`${id}_${model.model.uri.path.substring(1)}_${index}`}
