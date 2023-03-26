314276bcaabaa76faa960edbc0b0798e8dece772
GlacierLuo <1090490148@qq.com>
2023-03-26 21:59:41 +0800

    Add json display in console.

--- packages/core-dev/src/components/FooterConsole/ConsoleBlock.tsx
+++ packages/core-dev/src/components/FooterConsole/ConsoleBlock.tsx
@@ -1,20 +1,36 @@
-import {TabPanel} from "@material-tailwind/react";
+import { TabPanel } from "@material-tailwind/react";
+import ReactJson from 'react-json-view'
 
-import {useEditor} from "../../editor/editorContext";
-import {generateConsoleMessageToShow} from "../../types/console";
+import { useEditor } from "../../editor/editorContext";
+import { generateConsoleMessageToShow } from "../../types/console";
 
 const ConsoleBlock = () => {
-  const {id, state} = useEditor();
+  const { id, state } = useEditor();
 
   const consoleMessages = state.consoleMessages || [];
+  console.log(consoleMessages)
 
   return (
     <div key={id + "_consoleBlock"} className="flex-1 max-h-[220px] overflow-auto">
-      {consoleMessages.map((item, index) => (
-        <div key={index} className={`text-sm ${item.type === 'error' ? 'text-red-500' : 'text-white'}`}>
+      {consoleMessages.map((item, index) => {
+        let data
+        try {
+          data = JSON.parse(item.message)
+        } catch (e) { /* empty */ }
+        if (data instanceof Object) {
+          return (
+            <div key={index} className={`text-sm flex ${item.type === 'error' ? 'text-red-500' : 'text-white'}`}>
+              <span>[{new Date(item.timestamp).toLocaleTimeString()}]:</span>
+              <ReactJson src={data} theme="ocean" style={{backgroundColor: 'transparent'}} />
+            </div>
+          )
+        } else
+        return (
+          <div key={index} className={`text-sm ${item.type === 'error' ? 'text-red-500' : 'text-white'}`}>
             {generateConsoleMessageToShow(item).toString()}
-        </div>
-      ))}
+          </div>
+        )
+      })}
     </div>
   )
 }
