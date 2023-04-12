3b0006c141e63f996093a9da2abb9660657d03c1
GlacierLuo <1090490148@qq.com>
2023-04-12 16:21:27 +0800

    refactor: Add space between abi form and remove unused import.

--- packages/core/src/components-refactor/DeployAndCall/Call.tsx
+++ packages/core/src/components-refactor/DeployAndCall/Call.tsx
@@ -1,6 +1,3 @@
-import {useForm} from "react-hook-form";
-import {ContractInterface} from "ethers";
-import {useMemo, useRef} from "react";
 import { Disclosure } from '@headlessui/react'
 import {ChevronUpIcon} from "@heroicons/react/24/outline";
 
@@ -17,25 +14,27 @@ const Call = () => {
       <div className="mt-2">
         {compiledContracts.length === 0 ? <span className="text-primary-100">No contract has been deployed yet.</span> : ''}
         {compiledContracts.map((v) => {
-          return <Disclosure key={v.address}>
-            {({ open }) => (
-              <>
-                <Disclosure.Button className="relative box-border w-full py-2 pl-3 pr-10 rounded border-none text-white placeholder:text-[#878E95] text-left bg-[#36384A] focus:outline-none focus:shadow-outline">
-                  <span className="block truncate text-[12px] min-h-[12px]">{v.name}</span>
-                  <span className="h-fit pointer-events-none absolute inset-y-[3px] right-0 flex items-center pr-2">
+          return <div className="mt-2" key={v.address}>
+            <Disclosure key={v.address}>
+              {({ open }) => (
+                <>
+                  <Disclosure.Button className="relative box-border w-full py-2 pl-3 pr-10 rounded border-none text-white placeholder:text-[#878E95] text-left bg-[#36384A] focus:outline-none focus:shadow-outline">
+                    <span className="block truncate text-[12px] min-h-[12px]">{v.name}</span>
+                    <span className="h-fit pointer-events-none absolute inset-y-[3px] right-0 flex items-center pr-2">
                 <ChevronUpIcon
                   className={`${
                     open ? 'rotate-180 transform' : ''
                   } h-5 w-5`}
                 />
               </span>
-                </Disclosure.Button>
-                <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-primary-100">
-                  <AbiForm abi={v.abi} signerAddress={v.signerAddress} address={v.address} name={v.name} key={v.address} />
-                </Disclosure.Panel>
-              </>
-            )}
-          </Disclosure>
+                  </Disclosure.Button>
+                  <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-primary-100">
+                    <AbiForm abi={v.abi} signerAddress={v.signerAddress} address={v.address} name={v.name} key={v.address} />
+                  </Disclosure.Panel>
+                </>
+              )}
+            </Disclosure>
+          </div>
         })}
       </div>
     </div>
