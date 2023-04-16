4434339ffdb76d17b1eb8eb16a3a08e801f49273
chenchongqiang <chongqiangchen@gmail.com>
2023-04-16 14:46:56 +0800

    style(demo): update code style

--- apps/demo/src/index.tsx
+++ apps/demo/src/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
@@ -7,10 +8,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
-    <App />
+  <App />,
 );
 
 // If you want to start measuring performance in your app, pass a function
