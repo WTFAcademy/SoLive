ff69aca121e604414a74332751b621991077c94e
chenchongqiang <chongqiangchen@gmail.com>
2023-04-17 20:09:30 +0800

    doc: update doc

--- apps/demo/craco.config.js
+++ apps/demo/craco.config.js
@@ -1,5 +1,5 @@
 const cracoAlias = require("craco-alias");
-const webpack = require("webpack");
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   style: {
@@ -21,34 +21,8 @@ module.exports = {
   ],
   webpack: {
     configure: (webpackConfig, { env, paths }) => {
-      // eslint-disable-next-line no-param-reassign
-      webpackConfig.resolve.fallback = {
-        "http": require.resolve("stream-http"),
-        "https": require.resolve("https-browserify"),
-        "zlib": require.resolve("browserify-zlib"),
-        "stream": require.resolve("stream-browserify"),
-        "events": require.resolve("events/"),
-        "crypto": require.resolve("crypto-browserify"),
-        "assert": require.resolve("assert/"),
-        "buffer": require.resolve("buffer/"),
-        "util": require.resolve("util/"),
-        "path": require.resolve("path-browserify"),
-        "tty": require.resolve("tty-browserify"),
-        "os": require.resolve("os-browserify/browser"),
-        "punycode": require.resolve("punycode/"),
-        "fs": false,
-        "url": require.resolve("url/"),
-        "net": require.resolve("net-browserify"),
-        "child_process": false,
-        async_hooks: false,
-        rawBody: false,
-      };
-
-      webpackConfig.plugins.push(
-        new webpack.ProvidePlugin({
-          Buffer: ["buffer", "Buffer"]
-        })
-      );
+      webpackConfig.externalsPresets = {node: true};
+      webpackConfig.externals = [nodeExternals()];
       return webpackConfig;
     }
   }
