750d35e31ba5fc591abc5a2546eb4a2f1d97e7a7
Li, Amazing Ang <d0u.gie@hotmail.com>
2023-05-21 14:42:32 +0800

    Update docusaurus.config.js

--- apps/doc/docusaurus.config.js
+++ apps/doc/docusaurus.config.js
@@ -32,7 +32,7 @@ const SECTIONS = [
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "SoLive",
-  tagline: "SoLive is the first open-source lightweight Solidity Integrated Development Environment (IDE) that can be integrated into websites, documents, and tutorials easily. Powered by WTF Academy, Inspired by Remix-IDE and react-live.",
+  tagline: "The first open-source lightweight Solidity IDE that can be easily integrated into websites, documents, and tutorials. Powered by WTF Academy, Inspired by Remix-IDE and react-live.",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
